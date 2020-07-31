4215c7496e138f61089857b7c04039ed0091a745
Ithariel <jw+git@ichwillne.info>
2020-07-31 13:01:39 +0200

    Finish chkrootkit actions.py

--- chrootkit/actions.py
+++ chrootkit/actions.py
@@ -12,7 +12,21 @@ Suffix = "-1"
 WorkDir = "."
 
 def setup():
-    shelltools.system("ls -la")
+    shelltools.system("pwd")
+    shelltools.system("cd chkrootkit-0.53")
+    shelltools.system("make sense")
+    shelltools.system("cd ..")
 
 def install():
-    print("Test install")
+    pisitools.dodir("/usr/lib/chkrootkit")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/check_wtmpx")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/chkdirs")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/chklastlog")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/chkproc")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/chkutmp")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/chkwtmp")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/ifpromisc")
+    pisitools.insinto("/usr/lib/chrootkit/", "chkrootkit-0.53/strings-static")
+    pisitools.insinto("/usr/sbin/", "chkrootkit-0.53/chkrootkit")
+    pisitools.dosym("../lib/chkrootkit/chklastlog", "/usr/sbin/chklastlog")
+    pisitools.dosym("../lib/chkrootkit/chkwtmp", "/usr/sbin/chkwtmp")
